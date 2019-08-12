a2817246aa3ca36b4379edde91a9991aedac3d0a
nithish070 <54014189+nithish070@users.noreply.github.com>
2019-08-12 10:56:59 +0530

    Create possitivenegativezero.java

---
+++ possitivenegativezero.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class possitivenegativezero
+{
+Public static void main(string [] s)
+{
+int num;
+Scanner sc=new Scannner(System.in);
+System.out.print("enter the number:");
+num=sc.nextint();
+if(num>0)
+System.out.println(num+"is possitive number");
+else if(num<0)
+System.out.println(num+"is negative number");
+else
+System.out.println("its zero");
+}
+}
