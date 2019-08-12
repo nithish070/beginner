c7f09b53b16d1420f66bab94b83efd819412cafc
nithish070 <54014189+nithish070@users.noreply.github.com>
2019-08-12 11:51:55 +0530

    Update and rename oddoreven.java to odddoreven.java

---
+++ odddoreven.java
@@ -0,0 +1,15 @@
+import java.util.scanner;
+class odddoreven
+{
+Public static void main(string args[])
+{
+intx;
+System.out.println("enter an integer"):
+Scanner in=new Scanner(System.in);
+x=in.nextInt();
+if (x%2==0)
+System.out.println("the number is odd");
+else
+System.out.println("the number is even");
+}
+}
