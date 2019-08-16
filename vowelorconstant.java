96c4c8e087ad0eec17a190c23693313ef93fda10
nithish070 <54014189+nithish070@users.noreply.github.com>
2019-08-16 19:57:03 +0530

    Add files via upload

---
+++ vowelorconstant.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class vowelorconstant{
+
+public static void main(String[] args) {
+System.out.println("enter a character:");
+Scanner sc=new Scanner(System.in);
+char ch=sc.next().charAt(0);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+System.out.println("given character is vowel");
+}
+else
+{
+System.out.println("given character is constant");
+}
+}
+}
