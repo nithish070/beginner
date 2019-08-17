509471756c7fbbf1f83617bfa2242b6c0cef1a5e
nithish070 <54014189+nithish070@users.noreply.github.com>
2019-08-17 14:31:20 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class Prime {
+public static void main(String[] args) {
+int low = 20, high = 50;
+while (low < high) {
+boolean flag = false;
+for(int i = 2; i <= low/2; i++) {
+if(low % i == 0) {
+flag = true;
+break;
+}
+}
+if (!flag)
+System.out.print(low + " ");
+low++;
+}
+}
+}
